e8216152261a4078630bc4004a7a56253881aff2
vmolash <molash.v@gmail.com>
2020-09-24 15:05:36 -0700

    Updates

---
+++ precache-manifest.25d64d5613ef0170e7edd4ebaa51884e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9dfde997523d84e153e83c899e9852",
+    "url": "/counter_22sept/index.html"
+  },
+  {
+    "revision": "06e2fc874b32c19e1c18",
+    "url": "/counter_22sept/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fcf9ca16043966702d9a",
+    "url": "/counter_22sept/static/js/2.302ded10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter_22sept/static/js/2.302ded10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e2fc874b32c19e1c18",
+    "url": "/counter_22sept/static/js/main.1180cf82.chunk.js"
+  },
+  {
+    "revision": "af99d19abc4d61bc8d79",
+    "url": "/counter_22sept/static/js/runtime-main.14ba4ccc.js"
+  }
+]);
